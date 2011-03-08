900b3a8b3d58ea522f31996f540009f8ce753977
Martin Mauch <mauch@crealytics.de>
2011-03-09 00:45:42 +0100

    Remove generated classes.

--- info.cukes.feature/src-gen/info/cukes/feature/ResourceFactoryRegistration.java
+++
@@ -1,9 +0,0 @@
-package info.cukes.feature;
-
-import info.cukes.feature.resource.CucumberFeatureResourceFactory;
-
-public class ResourceFactoryRegistration {
-	public static void register() {
-	  	CucumberFeatureResourceFactory.register();
-	}
-}
